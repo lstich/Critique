a54789bb164764f35593e44dfe5e77d669348f1b
Luke Stichhaller <lstich@Lukes-MacBook-Pro.local>
2022-02-05 16:52:32 -0500

    server changes for register

--- routes/users.js
+++ routes/users.js
@@ -15,21 +15,41 @@ router.post("/login", function (req, res, next) {
       }
     },
     (err) => {
-      res.status(400).send("User not found");
+      res.status(400).send(err);
     }
   );
+  res.status(400).send("User not found!");
 });
 router.post("/register", function (req, res, next) {
   let { username, password } = req.body;
   User.findOne({ username: username }).then(
     (user) => {
       res.status(400).send("User already exists!");
+      return;
       //res.send({ albums });
     },
     (err) => {
-      res.status(400).send("User not found");
+      res.status(400).send(err);
     }
   );
+
+  //create and add new user
+
+  let newUser = new User({
+    username: username,
+    password: password,
+  });
+  newUser.save().then(
+    (user) => {
+      console.log("New User Added! ", user.username);
+    },
+    (err) => {
+      res.status(400).send(err);
+    }
+  ),
+    (err) => {
+      res.status(400).send(err);
+    };
 });
 
 router.post("/addAlbum", function (req, res, next) {});
