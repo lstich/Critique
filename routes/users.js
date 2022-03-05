22070cca5e02ccd0a10323c6d607728aaff9bc4d
Luke Stichhaller <lstich@Lukes-MacBook-Pro.local>
2022-03-05 15:26:52 -0500

    bcrypt 1

--- routes/users.js
+++ routes/users.js
@@ -17,11 +17,22 @@ router.post("/login", async function (req, res, next) {
 
   let existingUser = await User.findOne({ username: username });
   if (existingUser) {
-    if (password == existingUser.password) {
+    bcrypt.compare(passord, existingUser.password, function (err, result) {
+      console.log(result);
+      if (result == true) {
+        res.send("Logged in");
+        // The Password is Correct!
+      } else {
+        res.status(400).send("Password incorrect!");
+        // Your password is not correct.
+      }
+    });
+
+    /*if (password == existingUser.password) {
       res.send("Logged in");
     } else {
       res.status(400).send("Password incorrect!");
-    }
+    }*/
   } else {
     return res.status(400).send("User not found!");
   }
@@ -32,24 +43,31 @@ router.post("/register", async function (req, res, next) {
   let existingUser = await User.findOne({ username: username });
   if (existingUser) return res.status(400).send("User already exists!");
 
-  //create and add new user
+  //encrypt password
+  try {
+    bcrypt.hash(password, saltRounds, function (err, hash) {
 
-  let newUser = new User({
-    username: username,
-    password: password,
+      //create and add new user
+      let newUser = new User({
+        username: username,
+        password: hash,
+      });
+      newUser.save().then(
+        (user) => {
+          res.send("New User added! " + user);
+        },
+        (err) => {
+          res.status(400).send(err);
+        }
+      ),
+        (err) => {
+          res.status(400).send(err);
+        };
+    });
+  } catch (err) {
+    console.log(err)
   });
-  newUser.save().then(
-    (user) => {
-      res.send("New User added! " + user);
-    },
-    (err) => {
-      res.status(400).send(err);
-    }
-  ),
-    (err) => {
-      res.status(400).send(err);
-    };
-});
+  
 
 router.post("/changePassword", async function (req, res, next) {
   let { username, password } = req.body;
@@ -58,42 +76,32 @@ router.post("/changePassword", async function (req, res, next) {
   let existingUser = await User.findOne({ username: username });
   if (!existingUser) return res.status(400).send("User doesnt exist!");
 
-  //create and add new user
 
-  existingUser.password = password;
-  existingUser.save().then(
-    (user) => {
-      let rest = false;
-      try {
-        bcrypt.hash(p, saltRounds, function (err, hash) {
-          // Store hash in your password DB.
-          console.log(hash);
+  try {
+    bcrypt.hash(p, saltRounds, function (err, hash) {
+      // Store hash in your password DB.
+      console.log(hash);
 
-          bcrypt.compare(p, hash, function (err, result) {
-            console.log(result);
-            // result == true
-            if (result == true) {
-              rest = true;
-              // The Password is Correct!
-            } else {
-              rest = false;
-              // Your password is not correct.
-            }
-          });
-        });
-      } catch (err) {
-        console.log(err);
-      }
 
-      res.send("Password Successfully Changed! " + rest + " " + user);
-    },
-    (err) => {
-      res.status(400).send(err);
-    }
-  ),
-    (err) => {
-      res.status(400).send(err);
-    };
+       //create and add new user
+
+      existingUser.password = hash;
+      existingUser.save().then(
+        (user) => {
+          res.send("Password Successfully Changed! " + user);
+        },
+        (err) => {
+          res.status(400).send(err);
+        }
+      ),
+      (err) => {
+        res.status(400).send(err);
+      };
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
+ 
 
 module.exports = router;
